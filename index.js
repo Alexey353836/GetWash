850662b8a29c48e693f1332d1fc9839da8dcd772
Alexey353836 <127205134+Alexey353836@users.noreply.github.com>
2024-01-04 23:11:07 +0200

    burger menu and js

---
+++ index.js
@@ -0,0 +1,22 @@
+let burgerMenu = document.querySelector('.burger-menu'),
+    headerBurger = document.querySelectorAll('.header__burger')[1],
+    menuBtn = document.querySelectorAll('.header__burger')[0],
+    burgerContent = document.querySelector('.burger__content');
+   
+//  Open menu
+headerBurger.addEventListener ('click', function () {
+burgerMenu.style.visibility = 'visible';
+burgerMenu.style.opacity = '1';
+burgerContent.style. transform = 'translateX(0px)';
+});
+
+// Close menu
+menuBtn.addEventListener('click', function(){
+   
+    burgerContent.style. transform = '';
+    setTimeout(() => {
+        burgerMenu.style.visibility ='';
+        burgerMenu.style.opacity = '';
+      
+    }, 100);
+});
