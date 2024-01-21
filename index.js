f1655005698e4350de9a397b78a4fdc6bea30e55
Alexey353836 <example@gmail.com>
2024-01-21 12:21:05 +0200

    dd

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
-let burgerMenu = document.querySelector('.burger-menu'),
-    headerBurger = document.querySelectorAll('.header__burger')[1],
-    menuBtn = document.querySelectorAll('.header__burger')[0],
-    burgerContent = document.querySelector('.burger__content');
+'use strict';
+
+document.querySelector('.burger').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.header__nav').classList.toggle('open');
+})
+
+
+// let burgerMenu = document.querySelector('.burger-menu'),
+//     headerBurger = document.querySelectorAll('.header__burger')[1],
+//     menuBtn = document.querySelectorAll('.header__burger')[0],
+//     burgerContent = document.querySelector('.burger__content');
    
 //  Open menu
-headerBurger.addEventListener ('click', function () {
-burgerMenu.style.visibility = 'visible';
-burgerMenu.style.opacity = '1';
-burgerContent.style. transform = 'translateX(0px)';
-});
+// headerBurger.addEventListener ('click', function () {
+// burgerMenu.style.visibility = 'visible';
+// burgerMenu.style.opacity = '1';
+// burgerContent.style. transform = 'translateX(0px)';
+// });
 
 // Close menu
-menuBtn.addEventListener('click', function(){
+// menuBtn.addEventListener('click', function(){
    
-    burgerContent.style. transform = '';
-    setTimeout(() => {
-        burgerMenu.style.visibility ='';
-        burgerMenu.style.opacity = '';
+//     burgerContent.style. transform = '';
+//     setTimeout(() => {
+//         burgerMenu.style.visibility ='';
+//         burgerMenu.style.opacity = '';
       
-    }, 100);
-});
+//     }, 100);
+// });
