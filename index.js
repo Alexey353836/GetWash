b3c0cbb6b1626dd5f37df7eccc8e1f87cd980167
Alexey353836 <example@gmail.com>
2024-01-30 19:27:12 +0200

    enu

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
 'use strict';
 
+// Burger
 document.querySelector('.burger').addEventListener('click', function() {
     this.classList.toggle('active');
     document.querySelector('.burger-menu').classList.toggle('open');
     document.querySelector('.burger__content').classList.toggle('open');
 })
 
+// Menu
+const navLeft = document.querySelector('.nav__left'),
+     burgerNavBox1 = document.querySelector('.burger__nav-box-1');
 
-// let burgerMenu = document.querySelector('.burger-menu'),
-//     headerBurger = document.querySelectorAll('.header__burger')[1],
-//     menuBtn = document.querySelectorAll('.header__burger')[0],
-//     burgerContent = document.querySelector('.burger__content');
-   
-//  Open menu
-// headerBurger.addEventListener ('click', function () {
-// burgerMenu.style.visibility = 'visible';
-// burgerMenu.style.opacity = '1';
-// burgerContent.style. transform = 'translateX(0px)';
-// });
+const menuBox = {
+    menu:[
+        'Как использовать',
+        'Партнерам',
+        'Скачать',
+        'Отзывы',
+        'Контакты'
+    ]
+}
+menuBox.menu.sort();
+navLeft.innerHTML = '';
+burgerNavBox1.innerHTML = '';
+menuBox.menu.forEach((item) => {
+    navLeft.innerHTML += `
+    <a href="#" class="nav__left-text">${item}</a>
+    `;
+    burgerNavBox1.innerHTML += `
+    <a href="#" class="burger__text">${item}</a>
+    `;
+});
 
-// Close menu
-// menuBtn.addEventListener('click', function(){
-   
-//     burgerContent.style. transform = '';
-//     setTimeout(() => {
-//         burgerMenu.style.visibility ='';
-//         burgerMenu.style.opacity = '';
-      
-//     }, 100);
-// });
+
