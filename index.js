35017aea87ee07bd1efb0420d730336d03c81dcc
Alexey353836 <example@gmail.com>
2024-02-03 19:34:56 +0200

    burger menu close

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 'use strict';
 
 // Burger
-document.querySelector('.burger').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.burger-menu').classList.toggle('open');
-    document.querySelector('.burger__content').classList.toggle('open');
-})
+const burger = document.querySelector('.burger');
+burger.addEventListener('click', close);
+
+    function close() {
+        burger.classList.toggle('active');
+        document.querySelector('.burger-menu').classList.toggle('open');
+        document.querySelector('.burger__content').classList.toggle('open');
+}
+
 
 // Menu
+const black = document.querySelector('.black');
+const burgerNavClose = document.querySelector('.burger__nav-close');
+
+burgerNavClose.addEventListener("click", close); 
+black.addEventListener('click', close);
+
+
 const navLeft = document.querySelector('.nav__left'),
      burgerNavBox1 = document.querySelector('.burger__nav-box-1');
 const menuBox = {
