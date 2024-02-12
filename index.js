ae607c3517c7a098b4c8fc4b7e2aca27ac24a9f2
Alexey353836 <example@gmail.com>
2024-02-13 00:01:02 +0200

    hh

--- index.js
+++ index.js
@@ -2,29 +2,51 @@
 
 // Burger
 const burger = document.querySelector('.burger');
+const burgerOpen = document.querySelector('.burger-open');
 const burgerMenu = document.querySelector('.burger-menu');
+const burgerContent = document.querySelector('.burger__content');
+const burgerClose = document.querySelector('.burger-close');
+const close = document.querySelector('.close');
 
-burger.addEventListener('click', closeOpen);
 
-    function closeOpen () {
-        burger.classList.toggle('active');
-        document.querySelector('.burger-menu').classList.toggle('open');
-        document.querySelector('.burger__content').classList.toggle('open');
-      
+function openMenu(){
+    burgerMenu.classList.add('open');
+    burgerContent.classList.add('open'); 
+    burger.classList.remove('burger-open');
+    burger.classList.add('active');
+    document.body.style.overflow = 'hidden';
 }
 
+function closeMenu() {
+    burgerMenu.classList.remove('open');
+    burgerContent.classList.remove('open'); 
+    burger.classList.remove('active');
+    burger.classList.add('burger-open');
+    document.body.style.overflow = '';
+}
 
-// Burger menu
-const burgerNavClose = document.querySelector('.burger__nav-close');
+close.addEventListener('click', closeMenu);
 
-burgerNavClose.addEventListener("click", closeOpen); 
-burgerMenu.addEventListener('click', (event) => {
-    if (event.target && event.target.classList.contains('close')){
-        closeOpen ();
-        }
+burger.addEventListener('click', () => {
+    if (burger.classList.contains('burger-open')){
+        openMenu();
+    }else {
+        closeMenu();
+    }
 });
 
 
+// Burger menu
+
+burgerMenu.addEventListener('click', (e) => {
+    if (e.target && e.target.classList.contains('close')){
+        closeMenu ();
+        }
+    if (e.target && e.target === burgerMenu){
+        closeMenu();
+        }
+});
+    
 
 
 // Menu
