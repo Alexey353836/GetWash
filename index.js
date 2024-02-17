1a898ad8d9f90a5add2b15b7e06f57bed583e03e
Alexey353836 <example@gmail.com>
2024-02-17 16:24:38 +0200

    dd

--- index.js
+++ index.js
@@ -1,50 +1,92 @@
 'use strict';
 
+//scroll
+// Фиксированный обьект
+const header = document.querySelector('.header');
+// Фиксированный обьект end
+const scrollController = {
+   scrollPosition: 0,
+  disabledScroll() {
+      scrollController.scrollPosition = window.scrollY;
+      header.style.cssText = `
+    //   padding-right:20px;
+      padding-right: ${(window.innerWidth - document.body.offsetWidth + px)};
+    `;
+      document.body.style.cssText = `
+      overflow: hidden;
+      top:-${scrollController.scrollPosition}px;
+      left: 0;
+      padding-right: ${(window.innerWidth - document.body.offsetWidth + px)};
+      `;
+      document.documentElement.style.scrollBehavior = 'unset';
+  },
+  enabledScroll() {
+    window.scroll({top: scrollController.scrollPosition});
+    document.body.style.cssText = ``;
+    document.documentElement.style.scrollBehavior = '';
+    header.style.cssText = ` `;
+    document.body.style.cssText = ``;
+    },
+}
+//scroll end
+
+
+
+
 // Burger
 const burger = document.querySelector('.burger');
 const burgerOpen = document.querySelector('.burger-open');
 const burgerMenu = document.querySelector('.burger-menu');
+const burgerBody = document.querySelector('.burger__body');
 const burgerContent = document.querySelector('.burger__content');
 const burgerClose = document.querySelector('.burger-close');
-const close = document.querySelector('.close');
+const close = document.querySelectorAll('.close');
 
 
 function openMenu(){
     burgerMenu.classList.add('open');
     burgerContent.classList.add('open'); 
+    burgerBody.classList.add('open');
     burger.classList.remove('burger-open');
     burger.classList.add('active');
-    document.body.style.overflow = 'hidden';
+    scrollController.disabledScroll();
 }
 
 function closeMenu() {
     burgerMenu.classList.remove('open');
     burgerContent.classList.remove('open'); 
+    burgerBody.classList.remove('open');
     burger.classList.remove('active');
     burger.classList.add('burger-open');
-    document.body.style.overflow = '';
+    scrollController.enabledScroll();
 }
 
-burger.addEventListener('click', () => {
+function burgerOpenClose() {
     if (burger.classList.contains('burger-open')){
         openMenu();
     }else {
         closeMenu();
     }
-});
+}
 
-close.addEventListener('click', closeMenu);
+burger.addEventListener('click', burgerOpenClose);
+
+close.forEach(e => {
+    e.addEventListener('click', closeMenu);
+})
 
 // Burger menu
-burgerMenu.addEventListener('click', (e) => {
-    // if (e.target && e.target.classList.contains('close')){
-    //     closeMenu ();
-    //     }
-    if (e.target && e.target === burgerMenu){
-        closeMenu();
-        }
-});
+// burgerMenu.addEventListener('click', (e) => {
+//     if (e.target && e.target.classList.contains('close')){
+//         closeMenu ();
+//         }
+//     if (e.target && e.target === burgerMenu){
+//         closeMenu();
+//         }
+// });
+// Burger end
     
+
 
 
 // Menu
@@ -71,25 +113,25 @@ menuBox.menu.forEach((item) => {
     <div class="box_burger-text"><a href="#" class="burger__text">${item}</a></div>
     `;
 });
+// Menu end
+
+
 
 
 // Modal
-
 const btn = document.querySelectorAll('.button'),
     modal = document.querySelector('.modal'),
     modalDialog = document.querySelector('.modal__dialog'),
     modalClose = document.querySelector('.modal__close');
 
-// Открыть Модальное окно
-
 function openModal(){
+   burger.removeEventListener('click', burgerOpenClose);
    modal.classList.add('show');
    modalDialog.classList.add('show');
-   document.body.style.overflow = 'hidden';
-
+   scrollController.disabledScroll();
 }
 
-const modalTimerId = setTimeout(openModal, 5000);
+// const modalTimerId = setTimeout(openModal, 5000);
 
 function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.
@@ -105,12 +147,11 @@ btn.forEach(item => {
     item.addEventListener('click', openModal);
 });
 
- //  Закрыть модальное окно
-
 function closeModal() {
+    burger.addEventListener('click', burgerOpenClose );
     modal.classList.remove('show');
     modalDialog.classList.remove('show');
-    document.body.style.overflow = '';
+    scrollController.enabledScroll();
 }
 
 modalClose.addEventListener('click', closeModal);
@@ -119,5 +160,5 @@ modal.addEventListener('click', (e) => {
         closeModal();
     }
 });
-
+// Modal end
 
